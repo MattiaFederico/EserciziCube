bf5ad80062b6c69b56693d487d2ab50452925518
Mattia Federico <mattofede97@gmail.com>
2021-06-10 16:43:30 +0200

    Analisi UI5 completata

---
+++ Analisi-UI5-1/webapp/controller/Suppliers.controller.js
@@ -0,0 +1,61 @@
+sap.ui.define([
+  'App/controller/BaseController',
+  "sap/ui/core/Fragment"
+], function( BaseController, Fragment ) {
+  'use strict';
+  
+  BaseController.extend( "App.controller.Suppliers" , {
+    onInit: function() {
+
+    },
+    _getDialog : function () {
+      let oView = this.getView();
+      if (!this.pDialog) {
+				this.pDialog = Fragment.load({
+					id: oView.getId(),
+          name: "App.view.fragment.SuppliersDialog",
+          controller: this
+				}).then(function (oDialog) {
+					oView.addDependent(oDialog);
+					return oDialog;
+				});
+			}
+    },
+    onDialogOpen: function() {
+      this._getDialog();
+      this.pDialog.then((oDialog) => {
+        oDialog.open();
+      });
+    },
+    onDialogClose: function() {
+      this.byId("suppliersDialog").close();
+    },
+    onSubmit: function( oEvent ) {
+      let inputValue = parseInt(oEvent.getParameter("value"));
+      if( this.idChecker( inputValue ) ){
+        //Codice futuro
+      } else {
+        sap.m.MessageToast.show("Nessun fornitore associato");
+      }
+    },
+    liveChange: function( oEvent ) {
+      let regex = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
+      if( oEvent.getParameter("value").match(regex) ){
+        oEvent.getSource().setValue("");
+      }
+    },
+    idChecker: function(inputValue) {
+      let aSuppliers = this.getView().getModel("Suppliers").getData().Suppliers;
+      let validator = false;
+      aSuppliers.forEach( obj => {
+        if( inputValue === obj.SupplierID ){
+          validator = true;
+        }
+      });
+      return validator;
+    },
+    onSupplierSelect: function() {
+      //Codice futuro
+    }
+  });
+});
